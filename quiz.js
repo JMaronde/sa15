04ebec070cdd2c83fd41255646287ea1eb3f3a47
Jonathan Maronde <j2mike4@gmail.com>
2024-04-12 14:23:31 -0500

    Added the checkAnswers function to check each user inputted answer if they match up with the correct answer. Questions 2 and 5 have 2 acceptable answers.

--- quiz.js
+++ quiz.js
@@ -28,3 +28,51 @@ const setCorrect = element => { // if they got the question right
     inputControl.classList.add('correct'); // this added class will be for the CSS to highlight green 
     inputControl.classList.remove('incorrect');
 };
+
+const checkAnswers = () => {
+    const answer1Value = answers[0].value.trim();
+    const answer2Value = answers[1].value.trim();
+    const answer3Value = answers[2].value.trim();
+    const answer4Value = answers[3].value.trim();
+    const answer5Value = answers[4].value.trim();
+
+    if(answer1Value === '') {
+        setIncorrect(answers[0], 'The correct answer was Software');
+    } else if (answer1Value.toLowerCase() != 'software') {
+        setIncorrect(answers[0], 'The correct answer was Software');
+    } else {
+        setCorrect(answers[0]);
+    }
+
+    if(answer2Value === '') {
+        setIncorrect(answers[1], 'The correct answers were bicep or biceps');
+    } else if (answer2Value.toLowerCase() != 'bicep' && answer2Value.toLowerCase() != 'biceps') {
+        setIncorrect(answers[1], 'The correct answers were bicep or biceps');
+    } else {
+        setCorrect(answers[1]);
+    }
+
+    if(answer3Value === '') {
+        setIncorrect(answers[2], 'The correct answer was George Washington');
+    } else if (answer3Value.toLowerCase() != 'george washington') {
+        setIncorrect(answers[2], 'The correct answer was George Washington');
+    } else {
+        setCorrect(answers[2]);
+    }
+
+    if(answer4Value === '') {
+        setIncorrect(answers[3], 'The correct answer was 4');
+    } else if (answer4Value != 4) {
+        setIncorrect(answers[3], 'The correct answer was 4');
+    } else {
+        setCorrect(answers[3]);
+    }
+
+    if(answer5Value === '') {
+        setIncorrect(answers[4], 'The correct answers were Spongebob or Spongebob Squarepants');
+    } else if (answer5Value.toLowerCase() != 'spongebob' && answer5Value.toLowerCase() != 'spongebob squarepants') {
+        setIncorrect(answers[4], 'The correct answers were Spongebob or Spongebob Squarepants');
+    } else {
+        setCorrect(answers[4]);
+    }
+}
