5c5beec7d87c32219b3e7e4fbaff90970a2daad2
Jonathan Maronde <j2mike4@gmail.com>
2024-04-12 13:39:36 -0500

    Created two function in the js to add a message and add an incorrect class if there isn't already one, and the other one would add a correct class for the CSS styling later to be easier

--- quiz.js
+++ quiz.js
@@ -10,3 +10,21 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     checkAnswers();
 });
+
+const setIncorrect = (element, message) => { // if they got the question wrong
+    const inputControl = element.parentElement;
+    const incorrectDisplay = inputControl.querySelector('.incorrect');
+
+    incorrectDisplay.innerText = message; // displays a message if the wrong answer was put
+    inputControl.classList.add('incorect');
+    inputControl.classList.remove('correct')
+}
+
+const setCorrect = element => { // if they got the question right
+    const inputControl = element.parentElement;
+    const incorrectDisplay = inputControl.querySelector('.incorrect');
+
+    incorrectDisplay.innerText = '';
+    inputControl.classList.add('correct'); // this added class will be for the CSS to highlight green 
+    inputControl.classList.remove('incorrect');
+};
